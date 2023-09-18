e79a9c34451e32086e9346699992447daea042ea
kiok127523 <kiok127523@gmail.com>
2023-09-18 11:25:38 +0700

    test
    test

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,18 +2,19 @@ var express = require('express');
 var router = express.Router();
 const User = require("../models/User");
 
-router.get("/", async (req, res)=>{
+router.get("/", async (req, res) => {
     let name
     let role
-    if(signedIn){
-        await User.findById(signedIn).then((result)=>{
+    if (signedIn) {
+        await User.findById(signedIn).then((result) => {
             console.log(result);
             name = `${result.fname} ${result.lname}`
             role = result.role
         })
     }
-    res.render('admin/index', { name, role});
-router.get("/", (req, res)=>{
+    res.render('admin/index', { name, role });
+})
+router.get("/", (req, res) => {
     return res.render('admin/index')
 
 })
