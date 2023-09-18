2f79b81c005b8e67cdeae3024cd5910a1a54fbad
ArinSwagg <rakuzanoat@gmail.com>
2023-09-18 11:16:09 +0700

    admin config

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 const User = require("../models/User");
 
-router.get("/", (req, res)=>{
-    return res.render('admin/index')
+router.get("/", async (req, res)=>{
+    let name
+    let role
+    if(signedIn){
+        await User.findById(signedIn).then((result)=>{
+            console.log(result);
+            name = `${result.fname} ${result.lname}`
+            role = result.role
+        })
+    }
+    res.render('admin/index', { name, role});
 })
 
 module.exports = router;
