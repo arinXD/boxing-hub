9fd859d64dd3bb80e7e1f4039cec56d179d20cc1
ArinSwagg <rakuzanoat@gmail.com>
2023-09-21 15:18:07 +0700

    add event data

--- app.js
+++ app.js
@@ -67,15 +67,15 @@ app.use('/authen', signInMiddleware, authenRouter);
 app.use('/athletes', athleteRouter);
 
 app.get("/fetch/api", async (req, res) => {
-    const url = 'https://api.sportsdata.io/v3/mma/scores/json/Leagues?key=b3ccef7241c64316a5449c90efb8c1b9';
+    const url = 'https://api.sportsdata.io/v3/mma/scores/json/Schedule/UFC/2023?key=b3ccef7241c64316a5449c90efb8c1b9';
     fetch(url)
         .then((res) => res.json())
         .then((athletesJson) => {
             const JSONObject = JSON.parse(JSON.stringify(athletesJson))
-            fs.writeFile('ufcLeagues.json', JSON.stringify(JSONObject), (error) => {
+            fs.writeFile('./UFCdata/ufcEvents.json', JSON.stringify(JSONObject), (error) => {
                 if (error) throw error;
             });
-            return res.send("Fetch done!\nJSON file created successfully: output.csv")
+            return res.send("Fetch done!\nJSON file created successfully: Event.csv")
         }).catch((err) => {
             return res.send(err)
         })
