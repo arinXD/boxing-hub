c44bc33c62671461b91155197ceda8411b966fc1
ArinSwagg <rakuzanoat@gmail.com>
2023-09-22 01:47:34 +0700

    resolve

---
+++ routes/indexRouter.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const User = require("../models/User");
+
+/* GET home page. */
+router.get('/', async (req, res, next) => {
+    let name = ""
+    let role
+    if (signedIn) {
+        await User.findById(signedIn).then((result) => {
+            console.log(result);
+            name = `${result.fname} ${result.lname}`
+        })
+    }
+    res.render('index', {
+        name
+    });
+})
+
+
+router.get('/users', async (req, res, next) => {
+    let users
+    await User.find().then((result) => {
+        users = result
+    }).catch((err) => {
+        console.error(err)
+    })
+    return res.json(users)
+})
+
+
+module.exports = router;
